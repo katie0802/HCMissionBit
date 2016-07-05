ff8173e94ae00ad517b3fd6bfa586739d4cf80a5
katie0802 <1828523856@qq.com>
2016-07-05 11:02:58 -0700

    commit

--- Game/main.js
+++ Game/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1500,1000,Phaser.AUTO);
+var game = new Phaser.Game(0,0,Phaser.AUTO);
 
-game.state.add('state1',demo.state1);
-game.state.start('state1');
+game.state.add('tilemap.js',demo.tilemap.js);
+game.state.start('tilemap.js');
